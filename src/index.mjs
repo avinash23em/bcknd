9d79a83841b71c44b4c3f7174df4466ad487cdcc
avinash23em <ap23vi@gmail.com>
2025-04-28 10:18:14 +0530

    intial

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,18 @@
 import express from 'express';
 const app=express();
 const port=process.env.port || 3000;
+
+app.get("/",(req,res)=>{
+res.send("hellow world");
+});
+
+app.get("/api/users",(req,res)=>{
+res.send([
+    {id:1,username:"avi",displayName:"ap23"},
+    {id:2,username:"avi",displayName:"ap23"},
+    {id:3,username:"avi",displayName:"ap23"}
+])
+})
 app.listen(port,()=>{
-    console.log("listening on port")
+    console.log(`listening on port ${port}` )
 });
