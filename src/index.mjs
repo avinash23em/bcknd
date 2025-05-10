c50ee85f33ded9dd126100bb4a1b6d063e35c5cb
avinash23em <ap23vi@gmail.com>
2025-05-10 23:53:16 +0530

    intial

--- src/index.mjs
+++ src/index.mjs
@@ -5,6 +5,11 @@ const port=process.env.port || 3000;
 app.get("/",(req,res)=>{
 res.send("hellow world");
 });
+const mock=[ 
+    {id:1,username:"avi",displayName:"ap23"},
+    {id:2,username:"avi",displayName:"ap23"},
+    {id:3,username:"avi",displayName:"ap23"}
+]
 
 app.get("/api/users",(req,res)=>{
 res.send([
@@ -15,6 +20,12 @@ res.send([
 })
 app.get("/api/users/:id",(req,res)=>{
     console.log(req.params);
+    const parseId=parseInt(req.id);
+    if(isNaN(parseId)){
+        return res.sendStatus(400)
+    }
+    
+
 })
 app.get("api/users/product",(req,res)=>{
     const user={id:req.params.id,username:"avi",displayName:"ap23"}
