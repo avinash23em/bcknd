e3c989716b6c5e060946b8c1c30e6d221cf81362
avinash23em <ap23vi@gmail.com>
2025-05-25 14:39:26 +0530

    intial

--- src/index.mjs
+++ src/index.mjs
@@ -12,18 +12,18 @@ const mock=[
 ]
 
 app.get("/api/users",(req,res)=>{
-res.send([
-    {id:1,username:"avi",displayName:"ap23"},
-    {id:2,username:"avi",displayName:"ap23"},
-    {id:3,username:"avi",displayName:"ap23"}
-])
+res.send(mock);
 })
+
 app.get("/api/users/:id",(req,res)=>{
     console.log(req.params);
-    const parseId=parseInt(req.id);
+    const parseId=parseInt(req.params.id);
     if(isNaN(parseId)){
         return res.sendStatus(400)
     }
+    const finduser=mock.find((user)=>user.id==parseId);
+    if(!finduser) res.send(404);
+    return res.send(finduser);
     
 
 })
